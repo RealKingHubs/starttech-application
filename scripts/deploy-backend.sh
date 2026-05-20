88871fbfd3be5d10e8778ff8fcfba18331e6b345
RealKingHubs <odokingsleyu@gmail.com>
2026-05-20 10:57:18 +0100

    502 Badgateway new version of Deploy script

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -7,36 +7,29 @@ INSTANCE_IDS=$(aws ssm describe-instance-information \
   --query "InstanceInformationList[*].InstanceId" \
   --output text)
 
-# Fail fast if no instances are registered in SSM
-if [ -z "$INSTANCE_IDS" ]; then
-    echo "ERROR: No EC2 instances found registered in SSM Parameter Store/Managed Instances."
-    exit 1
-fi
-
 aws ssm send-command \
   --instance-ids $INSTANCE_IDS \
   --document-name "AWS-RunShellScript" \
   --region us-east-1 \
-  --parameters "commands=[
-    \"MONGO_URI=\$(aws ssm get-parameter --name /starttech/dev/mongo_uri --with-decryption --query Parameter.Value --output text --region us-east-1)\",
-    \"JWT_SECRET=\$(aws ssm get-parameter --name /starttech/dev/jwt_secret --with-decryption --query Parameter.Value --output text --region us-east-1)\",
-    \"DB_NAME=\$(aws ssm get-parameter --name /starttech/dev/db_name --query Parameter.Value --output text --region us-east-1)\",
-    
-    \"aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 093796422475.dkr.ecr.us-east-1.amazonaws.com\",
+  --parameters 'commands=[
+    "MONGO_URI=$(aws ssm get-parameter --name /starttech/dev/mongo_uri --with-decryption --query Parameter.Value --output text --region us-east-1)",
+    "JWT_SECRET=$(aws ssm get-parameter --name /starttech/dev/jwt_secret --with-decryption --query Parameter.Value --output text --region us-east-1)",
+    "DB_NAME=$(aws ssm get-parameter --name /starttech/dev/db_name --query Parameter.Value --output text --region us-east-1)",
 
-    \"docker system prune -af || true\",
-    \"docker pull $IMAGE\",
+    "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 093796422475.dkr.ecr.us-east-1.amazonaws.com",
 
-    \"docker stop backend || true\",
-    \"docker rm backend || true\",
+    "docker system prune -af || true",
+    "docker pull '"$IMAGE"'",
 
-    \"docker run -d --name backend -p 8080:8080 \\
-      -e PORT=8080 \\
-      -e MONGO_URI=\\\$MONGO_URI \\
-      -e DB_NAME=\\\$DB_NAME \\
-      -e JWT_SECRET_KEY=\\\$JWT_SECRET \\
-      -e ORIGINS_STRING='http://localhost:5173,http://amazonaws.com' \\
-      -e ENABLE_CACHE=true \\
-      -e REDIS_ADDR='dev-redis.yd4jsv.0001.use1.cache.amazonaws.com' \\
-      $IMAGE\"
-  ]"
+    "docker stop backend || true",
+    "docker rm backend || true",
+
+    "docker run -d --name backend -p 8080:8080 \
+      -e PORT=8080 \
+      -e MONGO_URI=$MONGO_URI \
+      -e DB_NAME=$DB_NAME \
+      -e JWT_SECRET_KEY=$JWT_SECRET \
+      -e ENABLE_CACHE=true \
+      -e REDIS_ADDR='dev-redis.yd4jsv.0001.use1.cache.amazonaws.com' \
+      '"$IMAGE"'"
+  ]'
